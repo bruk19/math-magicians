2ff7005ee9b0fe84cb5f01d5edf8cbe3fb4ea3f2
bruk19 <brukteshome21@gmail.com>
2022-06-29 18:28:15 +0300

    Add operators implimantaion

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,45 +1,56 @@
 import React from "react"
+import calculate from "../logic/calculate"
+import operate from "../logic/operate"
+
 class App extends React.Component {
     constructor(props) {
         super(props);
+        this.handleValueChange = this.handleValueChange.bind(this);
+        this.state = {value: ''};
      
     }
-    
+    handleValueChange(newValue) {
+        this.setState({value: newValue});
+
+    }
          createDigits = () => {
         const digits = [];
 
         for (let i = 9; i > 0; i--) {
             digits.push(
-                <button key={i}>{i}</button>
+                <button onClick={() => this.setState(i.toString())} key={i}>{i}</button>
             )
         }
         return digits;
     }
     
     render() {
-        return (<div className="App">
+        return (
+        
+        <div className="App">
         <div className="calculator">
             <div className="display">
-                <span>0</span>
+                {result ? <span>0</span> : '' }
+                { value }
             </div>
-
+          
             <div className="operators">
                 <div className="digits">
-                    <button>AC</button>
-                    <button>+/-</button>
-                    <button>%</button>
+                    <button onClick={() => calculate('AC')}>AC</button>
+                    <button onClick={() => operate('-')}>+/-</button>
+                    <button onClick={() => operate('%')}>%</button>
                    {this.createDigits()}
-                    <button className="btn0">0</button>
+                    <button  onClick={() => operate('+')} className="btn0">0</button>
                     <button></button>
-                    <button>.</button>
+                    <button onClick={() => operate('.')}>.</button>
                 </div>
 
                 <div className="symbol">
-                    <button>+</button>
-                    <button>*</button>
-                    <button>-</button>
-                    <button>+</button>
-                    <button>=</button>
+                    <button onClick={() => operation('+')}>+</button>
+                    <button onClick={() => operation('*')}>*</button>
+                    <button onClick={() => operation('-')}>-</button>
+                    <button onClick={() => operation('+')}>+</button>
+                    <button onClick={() => operation('=')}>=</button>
                 </div>
             </div>
         </div>
