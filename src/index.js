0daf9e59b8e6d50fe3df39f323c5b99b2fdd5345
bruk19 <brukteshome21@gmail.com>
2022-07-06 15:03:29 +0300

    Add nav to pages

--- src/index.js
+++ src/index.js
@@ -8,12 +8,10 @@ import './App.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
     <BrowserRouter>
       <App />
       <Apps />
     </BrowserRouter>
-    
   </React.StrictMode>
 );
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes, Route, Link } from "react-router-dom";
-
+<nav />
 function Home() {
     return (
       <>
